054fd1b012507a645df35228dedc4defc9ded192
PovOleksandr <70759845+PovOleksandr@users.noreply.github.com>
2021-05-20 16:37:55 +0300

    Add files via upload

--- spec/jasmine_examples/baseRequest.js
+++ spec/jasmine_examples/baseRequest.js
@@ -9,30 +9,25 @@ class BaseRequest{
         this.url = userUrl;
         this.header = userHeader;
         this.data = userData;
-        }
+    }
 
-        run(expectation){
+    run(expectation){
+        let resMethod = {method:this.method,
+                         url: this.url,
+                         headers: this.header,
+                         data: this.data};
 
-            let resMethod = {
-                method:this.method,
-                url: this.url,
-                headers: this.header,
-                data: this.data
-            };
+        it(expectation, async function() {
+            let responseStatus =0;
+            await axios(resMethod)
+                .then( function (response) {
+                    responseStatus  = response.status;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
 
-            it(expectation, async function() {
-                let responseStatus =0;
-                await axios(resMethod)
-                    .then( function (response) {
-                        responseStatus  = response.status;
-                    })
-                    .catch(function (error) {
-                        console.log(error);
-                    });
-
-                expect(responseStatus).toBe(200);
-
-            }, 10000);
+            expect(responseStatus).toBe(200);}, 10000);
         }
 }
 
