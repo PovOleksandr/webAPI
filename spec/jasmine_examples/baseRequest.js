88edc4786cee40fbeb2c724e496f040eb5bc5413
PovOleksandr <70759845+PovOleksandr@users.noreply.github.com>
2021-05-20 15:59:02 +0300

    Update baseRequest.js

--- spec/jasmine_examples/baseRequest.js
+++ spec/jasmine_examples/baseRequest.js
@@ -11,7 +11,7 @@ class BaseRequest{
             url: this.url,
             headers: this.header,
             data: this.data
-            }
+            };
         }
 
         getMethod(){return this.resMethod;}
